9b100b92c710388df9cb2e4ac2957cebb3fd8681
arcygore <rcgore602@gmail.com>
2023-01-16 20:14:27 -0500

    Updated with Traversy solution under my solution

--- 01_expanding_cards/script.js
+++ 01_expanding_cards/script.js
@@ -10,3 +10,20 @@ panels.forEach((panel) => {
         panel.classList.add('active');
     })
 })
+
+// Brad Traversy solution:
+
+// const panels = document.querySelectorAll('.panel')
+
+// panels.forEach((panel) => {
+//     panel.addEventListener('click', () => {
+//         removeActiveClasses()
+//         panel.classList.add('active')
+//     })
+// })
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
