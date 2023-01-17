f288376e75b651ca420255730a730967e6f02670
arcygore <rcgore602@gmail.com>
2023-01-16 23:27:12 -0500

    Updated progress steps with BT video solution

--- 03_progress_steps/script.js
+++ 03_progress_steps/script.js
@@ -37,3 +37,46 @@ next.addEventListener('click', () => {
     prev.disabled = false;
     progressBar.style.width = `${progressBarFill}%`;
 })
+
+// BT solution from video
+
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update() {
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
+}
